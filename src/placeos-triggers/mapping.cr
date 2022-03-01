565874478b8ebb36a144ee083a9f49fb78523203
Caspian Baska <email@caspian.computer>
2022-03-02 00:46:30 +1100

    refactor(mapping): centralise locking, protect methods

--- src/placeos-triggers/mapping.cr
+++ src/placeos-triggers/mapping.cr
@@ -11,87 +11,75 @@ module PlaceOS::Triggers
       trigger_instance_state[trigger_instance.id.as(String)]?
     end
 
-    # Instances
-    ###############################################################################################
-
     {% for verb in %w(add update remove) %}
       def {{ verb.id }}(model : Model::TriggerInstance | Model::Trigger)
-        case model
-        in Model::TriggerInstance then {{ verb.id }}_instance(model)
-        in Model::Trigger         then {{ verb.id }}_trigger(model)
+        mapping_lock.synchronize do
+          case model
+          in Model::TriggerInstance then {{ verb.id }}_instance(model)
+          in Model::Trigger         then {{ verb.id }}_trigger(model)
+          end
         end
       end
     {% end %}
 
-    def add_instance(instance : Model::TriggerInstance)
-      mapping_lock.synchronize do
-        trigger_id = instance.trigger_id.as(String)
-        trigger = trigger_cache[trigger_id]?
+    # TriggerInstances
+    ###############################################################################################
 
-        if trigger.nil?
-          trigger = trigger_cache[trigger_id] = instance.trigger.not_nil!
-        end
+    protected def add_instance(instance : Model::TriggerInstance)
+      trigger_id = instance.trigger_id.as(String)
+      trigger = trigger_cache[trigger_id]?
 
-        add_instance(trigger, instance)
+      if trigger.nil?
+        trigger = trigger_cache[trigger_id] = instance.trigger.not_nil!
+      end
+
+      add_instance(trigger, instance)
+    end
+
+    protected def add_instance(trigger : Model::Trigger, instance : Model::TriggerInstance)
+      State.new(trigger, instance).tap do |state|
+        self.trigger_instance_state[instance.id.as(String)] = state
+        self.trigger_state[trigger.id.as(String)] << state
       end
     end
 
-    def add_instance(trigger : Model::Trigger, instance : Model::TriggerInstance)
-      mapping_lock.synchronize do
-        State.new(trigger, instance).tap do |state|
-          self.trigger_instance_state[instance.id.as(String)] = state
-          self.trigger_state[trigger.id.as(String)] << state
-        end
-      end
+    protected def update_instance(instance : Model::TriggerInstance)
+      remove_instance(instance)
+      add_instance(instance)
     end
 
-    def update_instance(instance : Model::TriggerInstance)
-      mapping_lock.synchronize do
-        remove_instance(instance)
-        add_instance(instance)
-      end
-    end
-
-    def remove_instance(instance : Model::TriggerInstance)
-      mapping_lock.synchronize do
-        trigger_instance_state.delete(instance.id).try do |state|
-          self.trigger_state[instance.trigger_id]?.try &.delete(state)
-          state.terminate!
-        end
+    protected def remove_instance(instance : Model::TriggerInstance)
+      trigger_instance_state.delete(instance.id).try do |state|
+        self.trigger_state[instance.trigger_id]?.try &.delete(state)
+        state.terminate!
       end
     end
 
     # Triggers
     ###############################################################################################
 
-    def add_trigger(trigger : Model::Trigger)
-      mapping_lock.synchronize do
-        trigger_cache[trigger.id.as(String)] = trigger
+    protected def add_trigger(trigger : Model::Trigger)
+      trigger_cache[trigger.id.as(String)] = trigger
+    end
+
+    protected def update_trigger(trigger : Model::Trigger)
+      trigger_id = trigger.id.as(String)
+      self.trigger_cache[trigger_id] = trigger
+      self.trigger_state.delete(trigger_id).try &.each do |state|
+        instance = state.instance
+        self.trigger_instance_state.delete(instance.id)
+        state.terminate!
+
+        add_instance(trigger, instance)
       end
     end
 
-    def update_trigger(trigger : Model::Trigger)
+    protected def remove_trigger(trigger : Model::Trigger)
       trigger_id = trigger.id.as(String)
-      mapping_lock.synchronize do
-        self.trigger_cache[trigger_id] = trigger
-        self.trigger_state.delete(trigger_id).try &.each do |state|
-          instance = state.instance
-          self.trigger_instance_state.delete(instance.id)
-          state.terminate!
-
-          add_instance(trigger, instance)
-        end
-      end
-    end
-
-    def remove_trigger(trigger : Model::Trigger)
-      trigger_id = trigger.id.as(String)
-      mapping_lock.synchronize do
-        self.trigger_cache.delete trigger_id
-        self.trigger_state.delete(trigger_id).try &.each do |state|
-          self.trigger_instance_state.delete state.instance_id
-          state.terminate!
-        end
+      self.trigger_cache.delete trigger_id
+      self.trigger_state.delete(trigger_id).try &.each do |state|
+        self.trigger_instance_state.delete state.instance_id
+        state.terminate!
       end
     end
   end
