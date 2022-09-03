2a963f65894e20e6414b7fcd5242a436976bddea
Stephen von Takach <steve@place.technology>
2022-09-04 08:24:29 +1000

    feat: update to be self documenting

--- src/controllers/webhook.cr
+++ src/controllers/webhook.cr
@@ -2,29 +2,55 @@ module PlaceOS::Triggers::Api
   class Webhook < Application
     base "/api/triggers/v2/webhook"
 
-    before_action :find_hook
+    # =====================
+    # Filters
+    # =====================
 
-    @trigger : Model::TriggerInstance?
+    @[AC::Route::Filter(:before_action)]
+    def find_hook(id : String, secret : String)
+      # Find will raise a 404 (not found) if there is an error
+      trig = Model::TriggerInstance.find!(id)
 
-    class WebhookParams < ActiveModel::Model
-      include ActiveModel::Validation
-
-      attribute id : String
-      attribute secret : String
-
-      validates :id, presence: true
-      validates :secret, presence: true
-
-      def validate!
-        raise "missing trigger id or secret params" unless self.valid?
-        self
+      # Determine the validity of loaded TriggerInstance
+      if trig.enabled
+        if trig.webhook_secret == secret
+          @trigger = trig
+        else
+          message = "incorrect secret for trigger instance #{id}"
+          Log.warn { {
+            message:   message,
+            instance:  id,
+            trigger:   trig.trigger_id,
+            system_id: trig.control_system_id,
+          } }
+          raise Error::NotFound.new(message)
+        end
+      else
+        message = "webhook for disabled trigger instance #{id}"
+        Log.warn { {
+          message:   message,
+          instance:  id,
+          trigger:   trig.trigger_id,
+          system_id: trig.control_system_id,
+        } }
+        raise Error::NotFound.new(message)
       end
     end
 
-    # Return 204 if the state isn't loaded, might still be loading?
-    # 202 on success
-    def create
-      trig = @trigger.not_nil!
+    getter! trigger : Model::TriggerInstance
+
+    # =====================
+    # Routes
+    # =====================
+
+    # Informs the service that a webhook has been performed
+    # Return 204 if the state isn't loaded, 202 on success
+    @[AC::Route::POST("/:id", status: {
+      String => HTTP::Status::ACCEPTED,
+      Nil => HTTP::Status::NO_CONTENT,
+    })]
+    def create : String?
+      trig = trigger
       trigger_id = trig.id
       if state = Triggers.mapping.state_for? trig
         Log.debug { {
@@ -34,7 +60,7 @@ module PlaceOS::Triggers::Api
           system_id: trig.control_system_id,
         } }
         state.temporary_condition_met("webhook")
-        head :accepted
+        ""
       else
         Log.warn { {
           message:   "trigger state not loaded",
@@ -42,36 +68,7 @@ module PlaceOS::Triggers::Api
           trigger:   trig.trigger_id,
           system_id: trig.control_system_id,
         } }
-        head :no_content
-      end
-    end
-
-    def find_hook
-      # Find will raise a 404 (not found) if there is an error
-      args = WebhookParams.new(params).validate!
-      trig = Model::TriggerInstance.find!(args.id.not_nil!)
-
-      # Determine the validity of loaded TriggerInstance
-      if trig.enabled
-        if trig.webhook_secret == args.secret
-          @trigger = trig
-        else
-          Log.warn { {
-            message:   "incorrect secret for trigger instance #{args.id}",
-            instance:  args.id,
-            trigger:   trig.trigger_id,
-            system_id: trig.control_system_id,
-          } }
-          head :not_found
-        end
-      else
-        Log.warn { {
-          message:   "webhook for disabled trigger instance #{args.id}",
-          instance:  args.id,
-          trigger:   trig.trigger_id,
-          system_id: trig.control_system_id,
-        } }
-        head :not_found
+        nil
       end
     end
   end
