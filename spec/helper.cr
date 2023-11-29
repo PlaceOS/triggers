95144d217daaf2968b42c180ad2a06f7933b8b01
Ali Naqvi <naqvis@users.noreply.github.com>
2023-11-29 12:09:50 +0800

    feat: PPT-1038 Job to identify version upgrade for drivers (#44)

--- spec/helper.cr
+++ spec/helper.cr
@@ -31,96 +31,26 @@ require "../src/config"
 
 Spec.before_suite do
   PgORM::Database.configure { |_| }
-  # Triggers code
   Log.setup "*", :info, PlaceOS::LogBackend.log_backend
-  # We are creating them here for specs only. In normal cases, these
-  # model tables should have been created by init container or some other mechanism
-  PgORM::Database.connection do |db|
-    db.exec <<-SQL
-      DROP TABLE IF EXISTS "trigger"
-    SQL
-    db.exec <<-SQL
-      DROP TABLE IF EXISTS "sys"
-    SQL
-    db.exec <<-SQL
-      DROP TABLE IF EXISTS "trig"
-    SQL
-
-    db.exec <<-SQL
-      CREATE TABLE IF NOT EXISTS "trigger"(
-        created_at TIMESTAMPTZ NOT NULL,
-        updated_at TIMESTAMPTZ NOT NULL,
-        name TEXT NOT NULL,
-        description TEXT NOT NULL,
-        actions JSONB NOT NULL,
-        conditions JSONB NOT NULL,
-        debounce_period INTEGER NOT NULL,
-        important BOOLEAN NOT NULL,
-        enable_webhook BOOLEAN NOT NULL,
-        supported_methods TEXT[] NOT NULL,
-        control_system_id TEXT,
-        id TEXT NOT NULL PRIMARY KEY
-      );
-    SQL
-    db.exec <<-SQL
-    CREATE TABLE IF NOT EXISTS "sys"(
-      created_at TIMESTAMPTZ NOT NULL,
-      updated_at TIMESTAMPTZ NOT NULL,
-      name TEXT NOT NULL,
-      description TEXT NOT NULL,
-      features TEXT[] NOT NULL,
-      email JSONB,
-      bookable BOOLEAN NOT NULL,
-      display_name TEXT,
-      code TEXT,
-      type TEXT,
-      capacity INTEGER NOT NULL,
-      map_id TEXT,
-      images TEXT[] NOT NULL,
-      timezone TEXT,
-      support_url TEXT NOT NULL,
-      version INTEGER NOT NULL,
-      installed_ui_devices INTEGER NOT NULL,
-      zones TEXT[] NOT NULL,
-      modules TEXT[] NOT NULL,
-      id TEXT NOT NULL PRIMARY KEY,
-      public BOOLEAN NOT NULL DEFAULT false
-    );
-    SQL
-    db.exec <<-SQL
-    CREATE TABLE IF NOT EXISTS "trig"(
-      created_at TIMESTAMPTZ NOT NULL,
-      updated_at TIMESTAMPTZ NOT NULL,
-      enabled BOOLEAN NOT NULL,
-      triggered BOOLEAN NOT NULL,
-      important BOOLEAN NOT NULL,
-      exec_enabled BOOLEAN NOT NULL,
-      webhook_secret TEXT NOT NULL,
-      trigger_count INTEGER NOT NULL,
-      control_system_id TEXT,
-      trigger_id TEXT,
-      zone_id TEXT,
-      id TEXT NOT NULL PRIMARY KEY
-    );
-    SQL
-  end
+  clear_tables
 end
 
 Spec.before_each &->WebMock.reset
 
-# Clear test tables on exit
-Spec.after_suite do
-  PgORM::Database.connection do |db|
-    db.exec <<-SQL
-      DROP TABLE IF EXISTS "trigger"
-    SQL
-    db.exec <<-SQL
-      DROP TABLE IF EXISTS "sys"
-    SQL
-    db.exec <<-SQL
-      DROP TABLE IF EXISTS "trig"
-    SQL
-  end
+Spec.after_suite { clear_tables }
+
+def clear_tables
+  {% begin %}
+    Promise.all(
+      {% for t in {
+                    PlaceOS::Model::ControlSystem,
+                    PlaceOS::Model::Trigger,
+                    PlaceOS::Model::TriggerInstance,
+                  } %}
+        Promise.defer { {{t.id}}.clear },
+      {% end %}
+    ).get
+  {% end %}
 end
 
 module PlaceOS::Api::SpecClient
